c23294708a6e3b07f2ca476603b81e717d350e82
UrielX <panayotov.christian@gmail.com>
2019-06-20 11:36:27 +0300

    added games from invent games with python

--- python/softuni/python_basics/exams/food_delivery.py
+++
@@ -1,9 +0,0 @@
-chicken_menus = int(input()) * 10.35
-fish_menus = int(input()) * 12.40
-veggie_menus = int(input()) * 8.15
-
-total = chicken_menus + fish_menus + veggie_menus
-dessert = total * .20
-resut = total + dessert + 2.50
-
-print(f"Total: {resut:.2f}")
--- python/softuni/python_basics/exams/oscars_ceremony.py
+++
@@ -1,8 +0,0 @@
-hall_rent = int(input())
-
-statues = hall_rent - (hall_rent * .30)
-service = statues - (statues * .15)
-sound = service / 2
-total = statues + service + sound + hall_rent
-
-print(f"{total:.2f}")
