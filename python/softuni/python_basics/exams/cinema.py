161ed67132834a92b0eaa81c5ccf565d7697f7d8
UrielX <panayotov.christian@gmail.com>
2019-06-20 11:28:12 +0300

    solved the cinema problem

--- python/softuni/python_basics/exams/cinema.py
+++ python/softuni/python_basics/exams/cinema.py
@@ -1,28 +1,27 @@
-import sys
-cinema_capacity = int(input())
+capacity = int(input())
 
-total_profit = 0
-visitors = 0
-current_visitors = 0
-current_profit = 0
+cinema_profit = 0
 
-while visitors < cinema_capacity:
+while True:
     command = input()
 
     if command == "Movie time!":
-        print(f"There are {abs(visitors - cinema_capacity)} seats left in the cinema.")
-        print(f"Cinema income - {total_profit} lv.")
-        sys.exit()
+        print(f"There are {capacity} seats left in the cinema.")
+        break
+    
+    current_visitors = int(command)
 
-    visitors += int(command)
-    current_visitors += int(command)
-    current_profit += current_visitors * 5
+    if current_visitors > capacity:
+        print(f"The cinema is full.")
+        break
+    else:
+        capacity -= current_visitors
+
+    cinema_profit += current_visitors * 5
 
     if current_visitors % 3 == 0:
-        current_profit -= 5
-
-    total_profit += current_profit
+        cinema_profit -= 5
+    
     current_visitors = 0
-    current_profit = 0
 
-print(f"Cinema income - {total_profit} lv.")
+print(f"Cinema income - {cinema_profit} lv.")
