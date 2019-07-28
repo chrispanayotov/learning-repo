1054dd6ff03c2bc73f6dacd37644dce2e84af8e1
UrielX <panayotov.christian@gmail.com>
2019-07-28 11:41:56 +0300

    added lec4 python fundamentals

--- python/softuni/python_fundamentals/lection4/messages.py
+++ python/softuni/python_fundamentals/lection4/messages.py
@@ -1,12 +1,8 @@
 # WORK IN PROGRESS, CAN'T SOLVE FOR NOW
 
 class User:
-    def __init__(self, username, received_msgs):
+    def __init__(self, username):
         self.username = username
-        self.received_messages = received_msgs
-
-    def receive_msg(self):
-        pass
 
 
 class Message:
@@ -15,24 +11,32 @@ class Message:
         self.sender = sender
 
 
-data = input()
-users_to_register = []
-users_array = []
-received_msgs = []
-
-
 def create_user(username):
     new_user = User(username)
     return new_user
 
 
-while data[0] != 'exit':
+data = input()
+users_array = []
+correspondence_dict = {}
+
+while data != 'exit':
     if data[0] == 'r':
         data = data.split(' ')
-        users_to_register.append(data[1])
+        users_array.append(data[1])
     else:
         sender, _, receiver, message = data.split(' ')
+        correspondence_dict.setdefault((sender, receiver), [])
+        correspondence_dict[sender, receiver].append(message)
 
-
-        
     data = input()
+
+sender = []
+receiver = []
+
+for sender in correspondence_dict.keys():
+    # Goes through the messages of the first sender and then goes through
+    # the messages of the second sender. Need to figure a way how to print the
+    # first msg of the sender and then the response of the second participant
+    for i in range(len(correspondence_dict[sender])):
+        print(correspondence_dict[sender][i])
