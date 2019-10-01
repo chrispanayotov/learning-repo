e506e5e9e6ca45d2485a07e74160bcacc7e2686a
UrielX <panayotov.christian@gmail.com>
2019-10-01 13:54:20 +0300

    added solution to multiples of 3 or 5 problem

---
+++ javascript/codewars/multiples_3_or_5.js
@@ -0,0 +1,20 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number){
+    let numsArray = [];
+    let sum = 0;
+
+    for (let i = 0; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            numsArray.push(i);
+            sum += i
+        }
+    }
+    return sum;
+}
+
+solution(10); // => returns 23
+solution(20); // => returns 78
