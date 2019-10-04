86fe30ee154fe7b3b13841711978841ab6d97add
UrielX <panayotov.christian@gmail.com>
2019-10-04 12:27:15 +0300

    updated sort_array problem

--- javascript/softuni/fundamentals/lection4/sort_array.js
+++ javascript/softuni/fundamentals/lection4/sort_array.js
@@ -5,7 +5,12 @@
 // The input comes as an array of strings.
 
 function sortArray(arr) {
-    
+    arr.sort(function(a, b) {
+        return a.length - b.length || a.localeCompare(b);
+    })
+    for (let word of arr) {
+        console.log(word);
+    }
 }
 
-sortArray(['alpha', 'beta', 'gamma']) // => returns beta alpha gamma
+sortArray(['alpha', 'beta', 'gamma']); // => returns beta alpha gamma
